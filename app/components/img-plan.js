d8d61472a4a54946db0cc9b19b06308cee36f659
pijey <pjvilloud-bitbucket@yahoo.fr>
2016-08-27 18:41:54 +0200

    Fix scroll mapare

--- app/components/img-plan.js
+++ app/components/img-plan.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	didInsertElement(){
 		Ember.$('img[usemap]').rwdImageMaps();
+		Ember.$("area").click((e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			var speed = 750; // Durée de l'animation (en ms)
+			Ember.$('html, body').animate( { scrollTop: $(e.target.href.substring(e.target.href.lastIndexOf("#"))).offset().top - 65 }, speed );
+		});
 	}
 });
